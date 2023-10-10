701cc188a5b9b5d13483596be696d2978a3f2589
Eric Rodrigues de Oliveira <93745627+ericrodrigues00@users.noreply.github.com>
2023-10-10 03:43:22 -0300

    Update server.js 3.0

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// server.js
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -8,6 +9,7 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(cors()); // Habilita o CORS
+
 
 
 // Rota para verificar a validade do ingresso
@@ -34,48 +36,21 @@ app.get('/api/verificarIngresso', async (req, res) => {
 
 
 
-// Rota para verificar ingressos
-app.post('/api/verificar-ingresso', async (req, res) => {
+// Rota para registrar ingressos
+app.post('/api/ingressos', async (req, res) => {
   try {
-    const { qrCode } = req.body;
+    const { nome, contato } = req.body;
 
-    // Verifique se o ingresso com o QR code especificado existe
-    const ingresso = await Ingresso.findOne({ qrCode });
+    // Crie um novo ingresso no banco de dados
+    const novoIngresso = new Ingresso({ nome, contato });
 
-    if (ingresso) {
-      // Marque o ingresso como utilizado
-      ingresso.utilizado = true;
-      await ingresso.save();
-      res.status(200).json({ ingressoValido: true, numeroIngresso: ingresso.numeroIngresso });
-    } else {
-      res.status(200).json({ ingressoValido: false });
-    }
+    // Salve o ingresso no banco de dados
+    await novoIngresso.save();
+
+    res.status(201).json(novoIngresso);
   } catch (error) {
-    console.error('Erro ao verificar o ingresso:', error);
-    res.status(500).json({ error: 'Erro ao verificar o ingresso' });
-  }
-});
-
-// Rota para marcar ingressos como utilizados
-app.put('/api/marcar-utilizado/:numeroIngresso', async (req, res) => {
-  try {
-    const { numeroIngresso } = req.params;
-
-    // Encontre o ingresso pelo número do ingresso e marque como utilizado
-    const ingresso = await Ingresso.findOneAndUpdate(
-      { numeroIngresso },
-      { utilizado: true },
-      { new: true }
-    );
-
-    if (ingresso) {
-      res.status(200).json({ message: 'Ingresso marcado como utilizado com sucesso.' });
-    } else {
-      res.status(404).json({ error: 'Ingresso não encontrado.' });
-    }
-  } catch (error) {
-    console.error('Erro ao marcar o ingresso como utilizado:', error);
-    res.status(500).json({ error: 'Erro ao marcar o ingresso como utilizado' });
+    console.error('Erro ao registrar ingresso:', error);
+    res.status(500).json({ error: 'Erro ao registrar ingresso' });
   }
 });
 
@@ -84,3 +59,14 @@ const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`Servidor Express rodando na porta ${port}`);
 });
+
+
+app.get('/api/ingressos', async (req, res) => {
+    try {
+      const ingressos = await Ingresso.find(); // Recupere todos os ingressos do banco de dados
+      res.status(200).json(ingressos);
+    } catch (error) {
+      console.error('Erro ao buscar ingressos:', error);
+      res.status(500).json({ error: 'Erro ao buscar ingressos' });
+    }
+  });
